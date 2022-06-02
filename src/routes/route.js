6049abe0e8dee0699e1243473d43a22567a4236e
Ashwin5196@ <ashwinnikose180@gmail.com>
2022-06-02 17:13:44 +0530

    suceessfully done

--- src/routes/route.js
+++ src/routes/route.js
@@ -20,6 +20,35 @@ router.get('/test-me', function (req, res) {
 });
 
 
+router.get('/hello',function(req,res){
+   
+
+let year = ['JAN','FEB','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPT','OCT','NOV','DEC']
+let _= require("lodash");
+console.log(_.chunk(year,3))
+res.send('loadash installed')
+
+})
+let array =[1,3,5,7,9,11,13,15,17,19.]
+
+const _ = require('lodash');
+ let newArray = _.tail(array);
+  
+ console.log(newArray);
+
+let array1=[1,2,3,4,5]
+let array2=[1,3,4,6,7]
+let array3=[1,3,6,7,9]
+let array4=[3,4,6,7,9]
+let array5=[1,3,6,7,9]
+
+console.log(_.union(array1,array2,array3,array4,array5))
+
+let movie= [['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','fans Labyrinth']]
+console.log(_.fromPairs(movie))
+
+
+
 module.exports = router;
 
 
