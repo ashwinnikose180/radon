026e8fd0945d6668c1f10d83f4e9e1304d3ee5c7
Ashwin5196@ <ashwinnikose180@gmail.com>
2022-06-07 00:36:49 +0530

    Sucessfully done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,60 @@
-const { count } = require("console")
-const BookModel= require("../models/bookModel")
+const bookModel = require("../models/bookModel")
 
-const createBook= async function (req, res) {
-    let data= req.body
-
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+const createBook = async function (req,res){
+    let data=req.body
+    let savedData= await bookModel.create(data)
+    res.send({msg:savedData})
+}
+const bookList = async function(req,res){
+    let bookInfo =await bookModel.find().select( { bookName: 1, authorName: 1, _id: 0})
+    res.send({msg:bookInfo})
 }
 
-const getBooksData= async function (req, res) {
+const getBooksInYear = async function (req,res){
+    let data=req.query.year
+    let BooksInYear= await bookModel.find({year:year})
+    res.send({msg:BooksInYear})
+}
+
+const getParticularBooks = async function (req,res){
+    let condition=req.body
+    let ParticulatBooks= await bookModel.find(condition)
+    res.send({msg:ParticulatBooks})
+}
+
+const getRandomBooks = async function(req,res){
+    let randombooks =await bookModel.find({ $or :[ {totalPages:{ $gt: 500   }}]})
+    res.send({msg:randombooks})
+}
+const getXINRBooks = async function(req,res){
+    let INRBooks =await bookModel.find({"prices.indianPrice":{$in:["100INR", "200INR","500INR"]}})
+    res.send({msg:INRBooks})
+}
+
+module.exports.createBook=createBook
+module.exports.bookList=bookList
+module.exports.getBooksInYear=getBooksInYear
+module.exports.getParticularBooks=getParticularBooks
+module.exports.getRandomBooks=getRandomBooks
+module.exports.getXINRBooks=getXINRBooks
+
+
+
+
+
+
+
+// const { count } = require("console")
+// const BookModel= require("../models/bookModel")
+
+// const createBook= async function (req, res) {
+//     let data= req.body
+
+//     let savedData= await BookModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getBooksData= async function (req, res) {
 
     // let allBooks= await BookModel.find( ).count() // COUNT
 
@@ -65,21 +111,21 @@ const getBooksData= async function (req, res) {
     
     // ASYNC AWAIT
     
-    let a= 2+4
-    a= a + 10
-    console.log(a)
-    let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
+    // let a= 2+4
+    // a= a + 10
+    // console.log(a)
+    // let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
 
 
     // WHEN AWAIT IS USED: - database + axios
     //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
-    console.log(allBooks)
-    let b = 14
-    b= b+ 10
-    console.log(b)
-    res.send({msg: allBooks})
-}
+//     console.log(allBooks)
+//     let b = 14
+//     b= b+ 10
+//     console.log(b)
+//     res.send({msg: allBooks})
+// }
 
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+// module.exports.createBook= createBook
+// module.exports.getBooksData= getBooksData
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+const bookSchema = new mongoose.Schema({
+
+    bookName:{
+        type:String,
+        required: true
+},
+authorName:String,
+category :String,
+totalPages :Number,
+stockAvailable : Boolean,
+prices: {
+    indianPrice: String,
+    europePrice: String,
+},
+year:{
+    type:Number,
+    default:2021
+}},{timestamps:true}
+)
+
+module.exports = mongoose.model('Book', bookSchema) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const bookSchema = new mongoose.Schema( {
+//     bookName: String, 
+//     authorName: String, 
+//     tags: [String],
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+//     isPublished: Boolean,
+//     prices: {
+//         indianPrice: String,
+//         europePrice: String,
+//     },
+//     sales: {type: Number, default: 10}
+// }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+// module.exports = mongoose.model('Book', bookSchema) //users
 
 //Validation:
 //require:true
