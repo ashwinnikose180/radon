5f195378532378f7a31d702bfabd433a6c1756b1
Ashwin5196@ <ashwinnikose180@gmail.com>
2022-06-14 22:35:15 +0530

    Sucessfully done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -15,6 +15,42 @@ const userSchema = new mongoose.Schema( {
         enum: ["male", "female", "other"]
     },
     age: Number,
+    isDeleted : {
+        type: Boolean,
+        default: false
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+
+//         required: true
+//     },
+//     emailId: String,
+//     password: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "other"]
+//     },
+//     age: Number,
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema)
